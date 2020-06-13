7e8b948482b6c8122740be18d0cd7806b70e6fea
Moises Abreu <moises.rodrigues@unifametro.edu.br>
2020-06-12 20:49:31 -0600

    modulo de introducao a orientacao a objetos finalizado

---
+++ 1-Modulo/Introducao a Objetos - ByteBank/01-ByteBank/ContaCorrente.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    public class ContaCorrente
+    {
+        public  Cliente Titular { get; set; }
+        private int Agencia { get; set; } 
+        private int Numero { get; set; }    
+        private double _saldo;
+
+        // Propriedade que pertence somente a classe, todos os objetos compartilham dessa informação
+        public static int TotalDeContasCriadas {  get; private set; }
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        public static int GetTotalDeContasCriadas()
+        {
+            return TotalDeContasCriadas;
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            TotalDeContasCriadas++;
+        }
+
+        public bool Withdraw(Double value) 
+        {
+            if (_saldo < value)
+            {
+                return false;
+            }
+
+            _saldo -= value;
+            return true;
+        }
+
+        public void Deposit(Double value)
+        {
+            _saldo += value;
+        }
+
+        public bool Transfer(double value, ContaCorrente destinyAccount)
+        {
+            if (_saldo < value)
+            {
+                return false;
+            }
+
+            _saldo -= value;
+            destinyAccount.Deposit(value);
+            return true;
+        }
+    }
+}
---
+++ 1-Modulo/Introducao a Objetos - ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente moises = new Cliente();
+       
+            ContaCorrente conta = new ContaCorrente(5445, 54518-4);
+            conta.Titular = moises;
+            conta.Saldo = 150;
+
+            Console.WriteLine("Nome do titular" + conta.Titular.Nome);
+            Console.WriteLine("Total de contas criada: " + ContaCorrente.GetTotalDeContasCriadas());
+            Console.ReadLine();
+        }
+    }
+}
