6cd239eccd115f9b678ce4a7a9d536ecf506717d
Moises Rodrigues <moisesabreurodrigues@gmail.com>
2020-06-19 21:56:43 -0300

    modulo 2 - trabalhando com execoes iniciado

---
+++ 2-Modulo/Excecoes/01-ByteBank/ContaCorrente.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    public class ContaCorrente
+    {
+        public  Cliente Titular { get; set; }
+        private int Agencia { get; set; } 
+        private int Numero { get; set; }    
+        private double _saldo;
+
+        public static double TaxaOperacao;
+
+        // Propriedade que pertence somente a classe, todos os objetos compartilham dessa informação
+        public static int TotalDeContasCriadas {  get; private set; }
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        public static int GetTotalDeContasCriadas()
+        {
+            return TotalDeContasCriadas;
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            
+            TaxaOperacao = 30 / TotalDeContasCriadas;
+
+            TotalDeContasCriadas++;
+        }
+
+        public bool Withdraw(Double value) 
+        {
+            if (_saldo < value)
+            {
+                return false;
+            }
+
+            _saldo -= value;
+            return true;
+        }
+
+        public void Deposit(Double value)
+        {
+            _saldo += value;
+        }
+
+        public bool Transfer(double value, ContaCorrente destinyAccount)
+        {
+            if (_saldo < value)
+            {
+                return false;
+            }
+
+            _saldo -= value;
+            destinyAccount.Deposit(value);
+            return true;
+        }
+    }
+}
---
+++ 2-Modulo/Excecoes/01-ByteBank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            ContaCorrente conta = new ContaCorrente(5445, 54518-4);
+
+            Console.WriteLine(ContaCorrente.TaxaOperacao);
+            Console.ReadLine();
+        }
+    }
+}
